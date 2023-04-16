a5a8653819151682a26927da80dff158b8f209b7
LizRadds <avilradish@gmail.com>
2023-04-16 21:52:51 +0200

    fix: solve the half of the task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,8 +18,14 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  if (typeof sampleActivity !== 'string' || sampleActivity.trim() === '') {
+    return false;
+  }
+  const activity = parseFloat(sampleActivity);
+  if (isNaN(activity) || activity <= 0 || activity > MODERN_ACTIVITY) {
+  return false;
+  }
+
 }
 
 module.exports = {
