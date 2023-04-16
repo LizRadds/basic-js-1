66ddf2511ed1a69a8eb5a56c82b5700cd4ca5f02
LizRadds <avilradish@gmail.com>
2023-04-16 22:03:53 +0200

    fix: solve commomn-character and count-cats tasks

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,10 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const s1Letters = s1.split('');
+  let commonCount = 0;
+  for (let i = 0; i < s1Letters.length; i++) {
+    if (s2.includes(s1Letters[i])) {
+      commonCount++;
+      s2 = s2.replace(s1Letters[i], '');
+    }
+  }
+
+  return commonCount;
 }
+
 
 module.exports = {
   getCommonCharacterCount
